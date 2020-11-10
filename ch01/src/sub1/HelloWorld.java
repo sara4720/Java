5fb3cbdc1c5cfef5c624a97d3a7b762357a1f7b0
sara4720 <sara4720@naver.com>
2020-11-10 11:50:08 +0900

    2020/11/10

---
+++ ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/*
+ * 날짜: 2020/11/02
+ * 이름: 김사라
+ * 내용: ch01 자바 HelloWorld
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+
+}
