77395a9dac0c34562167ddedab067c8909762c3a
sara4720 <sara4720@naver.com>
2020-11-10 11:52:17 +0900

    2020/11/10

---
+++ ch02/src/p32/HelloJava.java
@@ -0,0 +1,24 @@
+package p32;
+
+/**
+ * 
+ * 날짜: 2020/11/02
+ * 이름: 김사라
+ * 내용: 교재 p32 따라하기
+ *
+ */
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		
+		/*
+		 * Hello Java를 출력하는 실행문을ㅇ 주석처리 하고
+		 * 아래 안녕 자바라고 출력되는 실행문을 추가
+		 */
+		
+		//"안녕 자바"라고 출력되는 실행문
+		System.out.println("안녕 자바");
+	}
+
+}
---
+++ ch02/src/sub1/HelloJava.java
@@ -0,0 +1,16 @@
+package sub1;
+
+/**
+ * 
+ * 날짜: 2020/11/02
+ * 이름: 김사라
+ * 내용: Ch02, 자바 설치 예제 교제 p31
+ *
+ */
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
